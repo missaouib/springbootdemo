12f3c4c99769ede384387bcbdfb18a02a698fa05
orionmeetartemis <orionmeetartemis@gmail.com>
2020-11-07 21:37:09 +0800

    add TestClass

---
+++ src/test/java/com/example/demo/NewTest.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NewTest {
+    public static void main(String[] args) {
+        String res = new String("eyJzc29JZCI6IjU2Nzg3NjcxMiIsImRldmljZUlkIjoiMzE4MjEzIiwibWFudWZhY3R1cmVyQ29kZSI6IjI5MzU5OTA5ODgyOCIsInByb2R1Y3RJZCI6IjE4MjgyMDQ1MTIzOTMxODUyOCIsImF0dGFjaG1lbnQiOm51bGx9");
+
+        System.out.println(res);
+    }
+}
