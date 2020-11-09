917d084b1a4cbf85f2f24b424d6cbb588ba3e278
orionmeetartemis <orionmeetartemis@gmail.com>
2020-11-09 20:34:00 +0800

    update lof4l2.xml

--- src/test/java/com/example/demo/NewTest.java
+++ src/test/java/com/example/demo/NewTest.java
@@ -2,11 +2,17 @@ package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class NewTest {
     public static void main(String[] args) {
         String res = new String("eyJzc29JZCI6IjU2Nzg3NjcxMiIsImRldmljZUlkIjoiMzE4MjEzIiwibWFudWZhY3R1cmVyQ29kZSI6IjI5MzU5OTA5ODgyOCIsInByb2R1Y3RJZCI6IjE4MjgyMDQ1MTIzOTMxODUyOCIsImF0dGFjaG1lbnQiOm51bGx9");
 
         System.out.println(res);
+        try {
+            throw new RuntimeException();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        System.out.println("after");
     }
 }
