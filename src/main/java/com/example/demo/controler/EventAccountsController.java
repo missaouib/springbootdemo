8c9e8eeb68e7c8a8308e9d4ecd5e8a47dcad56d1
orionmeetartemis <orionmeetartemis@gmail.com>
2020-11-24 16:46:18 +0800

    增加 spring event 事务监听器

--- src/main/java/com/example/demo/controler/EventAccountsController.java
+++ src/main/java/com/example/demo/controler/EventAccountsController.java
@@ -4,18 +4,20 @@ import com.example.demo.springevent.UserCreatedEvent;
 import com.example.demo.springevent.UserEventData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 @RestController
-public class AccountsController {
+public class EventAccountsController {
 
     @Autowired
     private ApplicationEventPublisher publisher;
 
     @GetMapping("/create")
+    @Transactional
     public void createUser(){
         publisher.publishEvent(new UserCreatedEvent(this,new UserEventData(new Date())));
     }
