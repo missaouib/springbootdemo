2a0311236c75e5886d220486f4e28a73645d3138
orionmeetartemis <orionmeetartemis@gmail.com>
2021-01-12 17:20:59 +0800

    add mybatis multiple-datasource

---
+++ src/main/java/com/example/demo/entity/Blog.java
@@ -0,0 +1,96 @@
+package com.example.demo.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Blog implements Serializable {
+    private Integer id;
+
+    private String author;
+
+    private String title;
+
+    private String subtitle;
+
+    private Date createtime;
+
+    private Date updatetime;
+
+    private String content;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author == null ? null : author.trim();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title == null ? null : title.trim();
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle == null ? null : subtitle.trim();
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getUpdatetime() {
+        return updatetime;
+    }
+
+    public void setUpdatetime(Date updatetime) {
+        this.updatetime = updatetime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", author=").append(author);
+        sb.append(", title=").append(title);
+        sb.append(", subtitle=").append(subtitle);
+        sb.append(", createtime=").append(createtime);
+        sb.append(", updatetime=").append(updatetime);
+        sb.append(", content=").append(content);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
---
+++ src/main/java/com/example/demo/mapper/secondary/BlogMapper.java
@@ -0,0 +1,38 @@
+package com.example.demo.mapper.secondary;
+
+import com.example.demo.entity.Blog;
+import com.example.demo.entity.BlogExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface BlogMapper {
+    long countByExample(BlogExample example);
+
+    int deleteByExample(BlogExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Blog record);
+
+    int insertSelective(Blog record);
+
+    List<Blog> selectByExampleWithBLOBs(BlogExample example);
+
+    List<Blog> selectByExample(BlogExample example);
+
+    Blog selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByExample(@Param("record") Blog record, @Param("example") BlogExample example);
+
+    int updateByPrimaryKeySelective(Blog record);
+
+    int updateByPrimaryKeyWithBLOBs(Blog record);
+
+    int updateByPrimaryKey(Blog record);
+}
