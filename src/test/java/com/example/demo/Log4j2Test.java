84f3e4ea07dea8485901d19a91fc071c73a759d2
orionmeetartemis <orionmeetartemis@gmail.com>
2021-03-18 11:17:27 +0800

    update log4j2 文件滚动删除

---
+++ src/test/java/com/example/demo/Log4j2Test.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class Log4j2Test {
+
+    @Test
+    public void test1() {
+        //通过打印i，日志文件中数字越小代表越老
+        for (int i = 0; i < 50000; i++) {
+            log.info("{}", i);
+            log.warn("{}", i);
+            log.debug("{}", i);
+            log.error("{}", i);
+        }
+
+    }
+}
