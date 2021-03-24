e0042c21b82358d723062ea57e4e85ec06ff1944
orionmeetartemis <orionmeetartemis@gmail.com>
2021-03-24 17:38:43 +0800

    update log4j2异步日志

--- src/test/java/com/example/demo/Log4j2Test.java
+++ src/test/java/com/example/demo/Log4j2Test.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.core.async.AsyncLoggerContextSelector;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,6 +12,7 @@ public class Log4j2Test {
     @Test
     public void test1() {
         //通过打印i，日志文件中数字越小代表越老
+        log.info("是否为异步日志：{}", AsyncLoggerContextSelector.isSelected());
         for (int i = 0; i < 50000; i++) {
             log.info("{}", i);
             log.warn("{}", i);
