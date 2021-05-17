b08bcfd5291686d5c1e2913d1f0758095cbcc74f
orionmeetartemis <orionmeetartemis@gmail.com>
2021-05-17 14:57:25 +0800

    update filter and interceptor

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.config;
+
+import com.example.demo.interceptor.Log2CostInterceptor;
+import com.example.demo.interceptor.LogCostInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogCostInterceptor())
+                .addPathPatterns("/**")
+                .order(0);
+        registry.addInterceptor(new Log2CostInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/book/selectall")
+                .order(1);
+
+    }
+}
